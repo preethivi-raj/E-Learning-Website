d96d1dbd195a67029c08906ad87591b4e5230d8e
PREETHIVIRAJ <preethiviraj9943@gmail.com>
2024-09-01 00:03:10 +0530

    production url chnages

--- frontend/src/baseUrl/baseUrl.js
+++ frontend/src/baseUrl/baseUrl.js
@@ -1,5 +1,6 @@
 
 
-const baseUrl = 'http://localhost:5000/api'
+//const baseUrl = 'http://localhost:5000/api'
+const baseUrl = 'https://e-learning-website-7tsw.onrender.com'
 
 export default baseUrl
